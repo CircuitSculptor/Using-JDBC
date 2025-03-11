1b0e9c000f32140f16c65a78379dd8b4a07a1b7e
BARTLOMIEJDRAPINSKI- <g00428194@atu.ie>
2025-03-11 09:58:56 +0000

    Working on TestConnection code

--- src/main/java/ie/atu/week6/TestConnection.java
+++ src/main/java/ie/atu/week6/TestConnection.java
@@ -1,4 +1,28 @@
 package ie.atu.week6;
+import java.sql.*;
 
 public class TestConnection {
+
+    public static void main(String[] args) {
+
+        Connection connection = null;
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydatabasedrapinski2", "root", "password");
+            System.out.println("Connection made to mydatabasedrapinski2");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        } finally
+        {
+            // Close the connection when finished
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
